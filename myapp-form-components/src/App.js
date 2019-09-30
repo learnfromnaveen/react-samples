32019a24edb2e2e0bbff0ab8ab47cdf3e6d33a29
naveen <learnfromnaveen@gmail.com>
2019-09-30 10:47:27 +0530

    React NameForm example

---
+++ myapp-form-components/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NameForm from "./components/name-form";
+
+class App extends React.Component {
+  render() {
+    return <NameForm />;
+  }
+}
+
+export default App;
---
+++ myapp-form-components/src/components/name-form.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstName: "",
+      lastName: ""
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    var key = e.target.name;
+    var newState = {};
+    newState[key] = e.target.value;
+
+    console.log(newState);
+
+    this.setState(newState);
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+    alert(
+      "You entered name: " + this.state.firstName + " " + this.state.lastName
+    );
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <div className="label">Firstname:</div>
+        <div className="control">
+          <input
+            type="text"
+            name="firstName"
+            value={this.state.firstName}
+            onChange={this.onChange}
+          />
+        </div>
+        <div className="label">Lastname:</div>
+        <div className="control">
+          <input
+            type="text"
+            name="lastName"
+            value={this.state.lastName}
+            onChange={this.onChange}
+          />
+        </div>
+        <div className="control">
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+    );
+  }
+}
+
+export default NameForm;
